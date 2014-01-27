e9788269e5a2a32f6347982167a364a5cf209e7c
Thiago Lovatine <thiagolovatine@mail.com>
2014-01-27 16:56:38 -0200

    2° commit :)

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Mobile2 | Mobile app</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link type="text/css" rel="stylesheet" href="assets/components/bootstrap/dist/css/bootstrap.css">
+	<link type="text/css" rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+<div class="container">
+
+      <form class="form-signin" role="form">
+        <h2 class="form-signin-heading">Área restrita:</h2>
+        <input type="text" class="form-control" placeholder="Email address" required autofocus>
+        <input type="password" class="form-control" placeholder="Password" required>
+        <label class="checkbox">
+          <input type="checkbox" value="remember-me"> Lembrar senha
+        </label>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+      </form>
+
+</div>
+
+<script type="text/javascript" src="assets/components/jquery/jquery.min.js"></script>
+<script type="text/javascript" src="assets/components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
